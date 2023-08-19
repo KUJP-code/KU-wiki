8831f7a7b9edcfcb15c5c5df2ad489ab4783f6d8
Paolo <paolo.decena@gmail.com>
2023-08-19 21:35:40 +0900

    Added some pages and changed titles

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
+      title: 'KU-Docs',
       social: {
         github: 'https://github.com/withastro/starlight',
       },
@@ -14,7 +14,8 @@ export default defineConfig({
           label: 'Guides',
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
+            //{ label: 'Example Guide', link: '/guides/example/' },
+            { label: 'Page Creation', link: '/guides/pagecreation'}
           ],
         },
         {
