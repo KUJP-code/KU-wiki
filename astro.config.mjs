40dae677f28f5da66780c782a7f48e68ba23fb5b
Brett Tanner <bretttanner171@gmail.com>
2023-09-21 15:36:22 +0900

    Add tailwind, upgrade to Astro 3 & Starlight 0.9

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,24 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: "My Docs",
-    social: {
-      github: "https://github.com/withastro/starlight"
-    },
-    sidebar: [{
-      label: "Guides",
-      autogenerate: {
-        directory: "guides"
-      }
-    }, {
-      label: "Reference",
-      autogenerate: {
-        directory: "reference"
-      }
-    }]
-  }), tailwind()]
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
