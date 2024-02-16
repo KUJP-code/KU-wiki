5deb0494d7d0fdc415d1c8c9e11d689856d7eaba
Brett Tanner <bretttanner171@gmail.com>
2024-02-16 11:56:22 +0900

    Update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,68 +1,73 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: {
-      entrypoint: "astro/assets/services/noop",
-    },
-  },
-  integrations: [
-    starlight({
-      title: "KU-Docs",
-      social: {
-        github: "https://github.com/Brett-Tanner/KU-wiki",
-      },
-      favicon: "/favicon.svg",
-      sidebar: [
-        {
-          label: "Guides",
-          collapsed: true,
-          autogenerate: { directory: "guides" },
-        },
-        {
-          label: "Materials Site",
-          collapsed: true,
-          autogenerate: { directory: "materials" },
-        },
-        {
-          label: "Registration Site",
-          collapsed: true,
-          items: [
-            {
-              label: "Overview",
-              collapsed: true,
-              link: "/registration_overview",
-            },
-            {
-              label: "AWS",
-              collapsed: true,
-              autogenerate: { directory: "aws" },
-            },
-            {
-              label: "Controllers",
-              collapsed: true,
-              autogenerate: { directory: "controllers" },
-            },
-            {
-              label: "Models",
-              collapsed: true,
-              autogenerate: { directory: "models" },
-            },
-            {
-              label: "Views",
-              collapsed: true,
-              autogenerate: { directory: "views" },
-            },
-          ],
-        },
-        {
-          label: "Setsumeikai Calendar",
-          collapsed: true,
-          autogenerate: { directory: "setsumeikai" },
-        },
-      ],
-    }),
-  ],
+	image: {
+		service: {
+			entrypoint: "astro/assets/services/noop",
+		},
+	},
+	integrations: [
+		starlight({
+			title: "KU-Docs",
+			customCss: ["./src/tailwind.css"],
+			social: {
+				github: "https://github.com/Brett-Tanner/KU-wiki",
+			},
+			favicon: "/favicon.svg",
+			sidebar: [
+				{
+					label: "Guides",
+					collapsed: true,
+					autogenerate: { directory: "guides" },
+				},
+				{
+					label: "Materials Site",
+					collapsed: true,
+					autogenerate: { directory: "materials" },
+				},
+				{
+					label: "Registration Site",
+					collapsed: true,
+					items: [
+						{
+							label: "Overview",
+							collapsed: true,
+							link: "/registration_overview",
+						},
+						{
+							label: "AWS",
+							collapsed: true,
+							autogenerate: { directory: "aws" },
+						},
+						{
+							label: "Controllers",
+							collapsed: true,
+							autogenerate: { directory: "controllers" },
+						},
+						{
+							label: "Models",
+							collapsed: true,
+							autogenerate: { directory: "models" },
+						},
+						{
+							label: "Views",
+							collapsed: true,
+							autogenerate: { directory: "views" },
+						},
+					],
+				},
+				{
+					label: "Setsumeikai Calendar",
+					collapsed: true,
+					autogenerate: { directory: "setsumeikai" },
+				},
+			],
+		}),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,30 @@
+import starlightPlugin from "@astrojs/starlight-tailwind";
+
+// Generated color palettes
+const accent = {
+	200: "#f4bb8c",
+	600: "#9d5800",
+	900: "#4c2800",
+	950: "#381c00",
+};
+const gray = {
+	100: "#eff8fb",
+	200: "#dff2f7",
+	300: "#b0c7cd",
+	400: "#6694a0",
+	500: "#32606b",
+	700: "#0e3f49",
+	800: "#002c35",
+	900: "#081b20",
+};
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: { accent, gray },
+		},
 	},
-	plugins: [],
-}
+	plugins: [starlightPlugin()],
+};
